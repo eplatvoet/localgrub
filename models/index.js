cf6dc55ca46817ddba9d09c0a26f9a6d948b35f0
kmvanneste <kmvanneste17@gmail.com>
2020-08-31 15:00:59 -0400

    added dbs and connected to localhost

--- models/index.js
+++ models/index.js
@@ -8,10 +8,12 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+require("dotenv").config();
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, process.env.MYSQL_KEY, config);
 }
 
 fs
