22af3cd55652d0cb9e21ba253caae1f743ae94d8
kmvanneste <kmvanneste17@gmail.com>
2020-09-01 13:45:20 -0400

    started to create the zomato ajax calls and JS functions

--- public/js/localCity.js
+++ public/js/localCity.js
@@ -1,49 +1,47 @@
 $(document).ready(function () {
 
-$("#city-search")
+// $("#city-search")
 //VARIABLES/Parameters
 var authKey = "MisterJayKayMap";
 
 //DYNAMICALLY CREATE BUTTONS BASED ON SEARCH
-  function createButton(citySearch) {
-    var newButton = $("<button>");
-    newButton.addClass("btn btn-secondary");
-    newButton.attr("type", "button");
-    newButton.html(citySearch);
-    $("#btn-group").prepend(newButton);
+  // function createButton(citySearch) {
+  //   var newButton = $("<button>");
+  //   newButton.addClass("btn btn-secondary");
+  //   newButton.attr("type", "button");
+  //   newButton.html(citySearch);
+  //   $("#btn-group").prepend(newButton);
 
-    newButton.on("click", function () {
-      citySearch = newButton.html();
-      newCurrentWeather = ``;
-      runCurrentQuery(newCurrentWeather);
-      $(".card").removeClass("hide");
-      //$(".future-forecast").removeClass("hide");
-    });
-  }
+  //   newButton.on("click", function () {
+  //     citySearch = newButton.html();
+  //     newCurrentWeather = ``;
+  //     runCurrentQuery(newCurrentWeather);
+  //     $(".card").removeClass("hide");
+  //     //$(".future-forecast").removeClass("hide");
+  //   });
+  // }
 
-  //Calling Map
-  function runCurrentQuery(queryURL) {
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
+//   //Calling Map
+//   function runCurrentQuery(queryURL) {
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET",
+//     }).then(function (response) {
 
-
-    });
-
-  }
-
-});
-
+//     });
+//   }
+// });
 
 //SEARCH BUTTON ON CLICK
 $("#city-search").on("click", function () {
   let cityName = $("#search").val().trim();
   let stateInitials = $("#search").val().trim();
   let radius = $("#search").val().trim();
-  let numOfMatches = $("#search").val().trim();
+  let numOfMatches = parseInt(10);
   let poi = $("#search").val().trim();
   
   var latestQuery = `https://www.mapquestapi.com/search/v2/radius?origin=${cityName},+${stateInitials}&radius=${radius}maxMatches=${numOfMatches}&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|581208&outFormat=json&key=${authKey}`;
-})
 
+  runCurrentQuery(latestQuery);
+})
+});
