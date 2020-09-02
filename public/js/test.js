91e928403e53e77c359e1aa09d5135855e4e9ec7
kmvanneste <kmvanneste17@gmail.com>
2020-09-02 17:57:34 -0400

    created updateReview function andassociated route

--- public/js/test.js
+++ public/js/test.js
@@ -1,5 +1,5 @@
-
-
+//DELETE 
+//add to front-end JS file
 function deleteBookmark (event) {
   //add deletebutton on click event here  
     event.stopPropagation();
@@ -13,7 +13,7 @@ function deleteBookmark (event) {
     })
 }
 
-//
+//add to API-routes
 app.delete("/api/restaurant/:id", (req, res) => {
     console.log("get /api/restaurant/")
     db.Restaurant.destroy({
@@ -28,3 +28,34 @@ app.delete("/api/restaurant/:id", (req, res) => {
       res.status(404).json(err);
     });
   })
+
+//---------------------------------------------------
+//UPDATE
+//add to front-end JS file
+function updateReview() {
+    //add button on click update 
+    var restaurantReview = ($(this).attr("id"));
+    $.put("/api/restaurant/" + restaurantReview)
+    .then((res) => {
+        console.log("update ", res);
+    }).catch((error) => {
+        console.error(error);  
+    })
+}
+
+//Update Route
+app.put("/api/restaurant/:id", (req, res) => {
+    console.log("put /api/restaurant/")
+    db.Restaurant.update({
+      where: {
+        id: req.params.id
+      },
+      include: [db.User]
+    })
+    .then(function(results) {
+      res.json(results);
+    }).catch(err => {
+      res.status(404).json(err);
+    });
+  })
+
