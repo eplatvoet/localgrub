372b466addf90562d76d5f92eeacee9cf333aedc
Jonghyun Kang <misterjaykay@gmail.com>
2020-09-02 16:54:45 -0400

    working to render information on main.js

--- public/js/main.js
+++ public/js/main.js
@@ -42,11 +42,35 @@ $(".bookmarks").on("click", "li", function () {
     console.log('id\n',$(this).attr("id"));
     $.get("/api/restaurant/" + restaurantID)
     .then((res) => {
-        console.log("result \n",res);
-     
+    console.log("result \n",res);
+    renderInfo(res.address, res.shop_name, res.shop_url);
+    renderImage(res.shop_image);
     }).catch((error) => {
         console.error(error);
       });
   })
+
+function renderInfo(address, name, url) {
+    $("#shop-details").empty();
+    var title = $("<h4>");
+    var pElOne = $("<p>");
+    var pElTwo = $("<p>");
+
+    $(title).text(name);
+    $(pElOne).text(address);
+    $(pElTwo).text(url);
+
+    $("#shop-details").append(title);
+    $("#shop-details").append(pElOne);
+    $("#shop-details").append(pElTwo);
+}
+
+function renderImage(image) {
+    $("#shop-image").empty();
+    var imgEl = $("<img>");
+    $(imgEl).attr("src", image);
+    $(imgEl).attr("class", "card-img");
+    $("#shop-image").append(imgEl);
+}
 
 getUserName();
