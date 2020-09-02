f217e65b3567ae93568001463936ea558e37657c
kmvanneste <kmvanneste17@gmail.com>
2020-09-02 16:29:47 -0400

    completed delete route for bookmarks

---
+++ public/js/test.js
@@ -0,0 +1,30 @@
+
+
+function deleteBookmark (event) {
+  //add deletebutton on click event here  
+    event.stopPropagation();
+    var restaurantID = ($(this).attr("id"));
+    $.get("/api/restaurant/" + restaurantID)
+    .then((res) => {
+        console.log("result ", res);
+
+    }).catch((error) => {
+        console.error(error);
+    })
+}
+
+//
+app.delete("/api/restaurant/:id", (req, res) => {
+    console.log("get /api/restaurant/")
+    db.Restaurant.destroy({
+      where: {
+        id: req.params.id
+      },
+      include: [db.User]
+    })
+    .then(function(results) {
+      res.json(results);
+    }).catch(err => {
+      res.status(404).json(err);
+    });
+  })
